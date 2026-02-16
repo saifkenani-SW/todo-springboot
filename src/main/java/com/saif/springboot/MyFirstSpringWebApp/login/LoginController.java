c617b2f8348d95590a8a360f3fa54c6f059c92c5
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-02-16 10:19:30 +0300

    Adding Logger in controller and app.prop git add src/main/resources/application.properties

--- src/main/java/com/saif/springboot/MyFirstSpringWebApp/login/LoginController.java
+++ src/main/java/com/saif/springboot/MyFirstSpringWebApp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.saif.springboot.MyFirstSpringWebApp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 
 public class LoginController {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping({"login"})
     public String goToLoginPage(@RequestParam String name, ModelMap model) {
+        logger.debug("Request param is {} ", name);
+        logger.info("I want this printed in info level ");
+        logger.warn("I want this printed in warn level");
+
         model.put("name", name);
         System.out.println(name + "Not Recommended");
         return "login";
