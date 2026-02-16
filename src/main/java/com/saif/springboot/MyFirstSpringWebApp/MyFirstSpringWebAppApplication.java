c1c6ab8eb3e3722d6443bbf3040658f98dec08c2
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-02-16 08:45:32 +0300

    init project

---
+++ src/main/java/com/saif/springboot/MyFirstSpringWebApp/MyFirstSpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.saif.springboot.MyFirstSpringWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saif/springboot/MyFirstSpringWebApp/MyFirstSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saif.springboot.MyFirstSpringWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
